7a8247eba7a422d403dad3b689f52e9454b3a756
andresito87 <andrespodadera87@gmail.com>
2022-09-10 17:27:23 +0200

    adding task8 python

---
+++ Curso Python/ModulesTask/Main.py
@@ -0,0 +1,16 @@
+# En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
+
+# Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.
+
+from modules.operations import *
+
+
+suma = suma(4, 5)
+resta = resta(4, 5)
+multiplicacion = multiplicacion(4, 5)
+division = division(4, 5)
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
