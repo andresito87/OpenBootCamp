626cd5eae4e738c99545eeb223e4f42b021f80a8
Andres <andresito87>
2022-10-09 13:39:40 +0200

    adding task2

---
+++ Curso de JavaScript Basico/Task2/index.js
@@ -0,0 +1,15 @@
+/*Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) */
+
+miLista = ["Andres", 35, true, new Date(), { titulo: "Clean Code", autor: "Robert C. Martin", fecha: new Date(2008, 8, 1), url: "https://www.amazon.es/C%C3%B3digo-Limpio-desarrollo-software-Programaci%C3%B3n/dp/8441532109/" }];
+
+console.log(miLista);
