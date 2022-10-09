8c06484061c6449f767b65c4b4f8dcd18f8428bf
Andres <andresito87>
2022-10-09 14:46:45 +0200

    adding task3

---
+++ Curso de JavaScript Basico/Task3/factorial-break.js
@@ -0,0 +1,14 @@
+//- factorial-break.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while, una bifurcación if y una sentencia break
+
+let factorial = 1;
+let i = 1;
+
+while (true) {
+    factorial *= i;
+    i++;
+    if (i > 10) {
+        break;
+    }
+}
+
+console.log(factorial);
---
+++ Curso de JavaScript Basico/Task3/factorial-for.js
@@ -0,0 +1,10 @@
+// - factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+
+
+let factorial = 1;
+
+for (i = 1; i <= 10; i++) {
+    factorial *= i;
+}
+
+console.log(factorial);
