8898f2afa55ee6f1be760ca30431745832bf260e
Andres <andresito87>
2023-04-07 15:47:29 +0200

    modify task2

--- Curso de Java Basico/Tema2/src/Main.java
+++ Curso de Java Basico/Tema2/src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        //modificacion
+        double price = 120;
+        double priceWithIva = calculatePriceWithIva(price);
+        System.out.println(priceWithIva);
+
+    }
+    public static double calculatePriceWithIva(double price) {
+        double tax = 0.21;
+        return price + price * tax;
     }
 }
