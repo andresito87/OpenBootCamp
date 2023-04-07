a4172b7cfb25b60aaa524762ef4a452e9ad2275e
Andres <andresito87>
2023-04-07 14:39:11 +0200

    adding package to task1 structure project

--- Curso de Java Basico/Tema1/src/Main.java
+++ Curso de Java Basico/Tema1/src/Main.java
@@ -1,25 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 5;
-        char b = 'a';
-        String c = "Hello";
-        double d = 2.2;
-        float e = 5.5f;
-        boolean f = true;
-        long g = 53333333333333333L;
-        short h = 7;
-        byte i = 5;
-
-        System.out.println("int: " + a);
-        System.out.println("char: " + b);
-        System.out.println("String: " + c);
-        System.out.println("double: " + d);
-        System.out.println("float: " + e);
-        System.out.println("boolean: " + f);
-        System.out.println("long: " + g);
-        System.out.println("short: " + h);
-        System.out.println("byte: " + i);
 
 
     }
